c4a77e8d19efe3dbaad38b7c180b91eb821555b4
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-12 18:04:21 -0300

    aula 1

---
+++ aula1/src/Exer2.java
@@ -0,0 +1,8 @@
+public class Exer2 {
+    public static void main(String[] args){
+        System.out.println("Renan Muniz merlo");
+        System.out.println("Rua Tanganica, 1500");
+        System.out.println("Celular: 98826-7864");
+        System.out.println("Cidade: Londrina");
+    }
+}
---
+++ aula1/src/Exer3.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Exer3 {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Ola mundo Java");
+        System.exit(0);
+    }
+
+}
