7e5a6ec5f99d0cd526cc9300296ea0b1a31482bc
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-12 17:30:56 -0300

    Exer2

---
+++ Exer2/src/Exer2.java
@@ -0,0 +1,8 @@
+public class Exer2 {
+    public static void main(String[] args){
+        System.out.println("Jõao paulo da Silva");
+        System.out.println("Rua das Flores, 1234");
+        System.out.println("Celular: 8740-4012");
+        System.out.println("Cidade: Londrina");
+    }
+}
