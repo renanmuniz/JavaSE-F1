184613e9d9bb46c455360b4536a7ea3ea987f05a
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-12 19:43:24 -0300

    Aula 2 Exer7

---
+++ aula1/src/Exer7.java
@@ -0,0 +1,10 @@
+public class Exer7 {
+    public static void main(String[] args) {
+        String nome = "Renan";
+        System.out.println(nome);
+
+        String sobre = "Merlo".toUpperCase();
+        System.out.println(sobre);
+        //System.out.println(nome + ' ' + sobre);
+    }
+}
