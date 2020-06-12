cc07b2d5b4cd36d93380a5f6d372c76ad9beb7e3
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-12 20:14:22 -0300

    Aula 2 Exer9

---
+++ aula1/src/Exer9.java
@@ -0,0 +1,10 @@
+public class Exer9 {
+    public static void main(String[] args) {
+        String nome="Renan";
+        String sobre="Muniz Merlo";
+        String completo=nome + ' ' + sobre;
+        System.out.println(completo);
+        String silva = completo + " da Silva";
+        System.out.println(silva);
+    }
+}
