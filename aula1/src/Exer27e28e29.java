74317360a4c32ee32b3ab844f9ac066aa4bcf472
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-13 16:07:30 -0300

    Aula 4 Exer27e28e29

---
+++ aula1/src/Exer27e28e29.java
@@ -0,0 +1,74 @@
+public class Exer27e28e29 {
+    public static void main(String[] args) {
+        int valor=20;
+        switch(valor){
+            case 10:{
+                System.out.println("é 10");
+                System.out.println("primeiro");
+                break;
+            }
+            case 20:{
+                System.out.println("é 20");
+                System.out.println("segundo");
+                break;
+            }
+            case 22:{
+                System.out.println("é 22");
+                System.out.println("terceiro");
+                break;
+            }
+            default:{
+                System.out.println("Não é nenhum valor esperado");
+            }
+        }
+        System.out.println("programa continua 1");
+
+        valor=10;
+        switch(valor){
+            case 10:{
+                System.out.println("é 10");
+                System.out.println("primeiro");
+                break;
+            }
+            case 20:{
+                System.out.println("é 20");
+                System.out.println("segundo");
+                break;
+            }
+            case 22:{
+                System.out.println("é 22");
+                System.out.println("terceiro");
+                break;
+            }
+            default:{
+                System.out.println("Não é nenhum valor esperado");
+            }
+        }
+        System.out.println("programa continua 2");
+
+        valor=50;
+        switch(valor){
+            case 10:{
+                System.out.println("é 10");
+                System.out.println("primeiro");
+                break;
+            }
+            case 20:{
+                System.out.println("é 20");
+                System.out.println("segundo");
+                break;
+            }
+            case 22:{
+                System.out.println("é 22");
+                System.out.println("terceiro");
+                break;
+            }
+            default:{
+                System.out.println("Não é nenhum valor esperado");
+            }
+        }
+        System.out.println("programa continua 3");
+
+
+    }
+}
