9bcdc4e7db0fc333f09da1c6ada94385a40193ab
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-12 21:06:59 -0300

    Aula 2 Exer13 e Exer14

---
+++ aula1/src/Exer13.java
@@ -0,0 +1,11 @@
+public class Exer13 {
+    public static void main(String[] args) {
+        int nove=9;
+        int dez=10;
+
+        System.out.println(dez>nove && dez>8);
+        System.out.println(dez>9 || dez<8);
+        System.out.println(dez>9);
+        System.out.println(!(dez>9));
+    }
+}
---
+++ aula1/src/Exer14.java
@@ -0,0 +1,26 @@
+public class Exer14 {
+    public static void main(String[] args) {
+        int a=10;
+        a=a+a;
+        System.out.println(a);
+
+        a=10;
+        a+=a;
+        System.out.println(a);
+
+        a=10;
+        a*=a;
+        System.out.println(a);
+
+        a=10;
+        a-=a;
+        System.out.println(a);
+
+        a/=2;
+        System.out.println(a);
+
+        a=7;
+        a%=2;
+        System.out.println(a);
+    }
+}
