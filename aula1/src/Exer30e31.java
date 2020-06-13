43097602e5a5a2b73dea6ed3ccfb8abe1c95a97b
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-13 16:25:47 -0300

    Aula 4 Exer30e31

---
+++ aula1/src/Exer30e31.java
@@ -0,0 +1,25 @@
+public class Exer30e31 {
+    public static void main(String[] args) {
+        //Exercicio30
+        int valor=10;
+        System.out.println(valor);
+        if(valor==10){
+            int v2=90;
+            System.out.println(v2);
+            System.out.println(valor);
+        }else{
+            //System.out.println(v2);
+            System.out.println(valor);
+        }
+        //System.out.println(v2);
+
+        //Exercicio31
+        byte v2=10;
+        System.out.println(v2);
+        {
+            int a=90;
+            System.out.println(a);
+        }
+        //System.out.println(a);
+    }
+}
